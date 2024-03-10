22cf17a8f4b0ce78f827a2d9f669e8559edcc006
romelgomez <bmxandcode@gmail.com>
2024-03-10 00:21:14 -0500

    Initial Commit

---
+++ Slots Game/Slots_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slots_GameApp.swift
+//  Slots Game
+//
+//  Created by romel javier gomez herrera on 10/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Slots_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
